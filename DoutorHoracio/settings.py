070b0683fe56602278c063f641e61f2841000dd0
ViniciusForti <vforti19@gmail.com>
2021-06-21 18:30:54 -0300

    Commit

--- DoutorHoracio/settings.py
+++ DoutorHoracio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,4 +152,5 @@ EMAIL_HOST_PASSWORD = "drhoracio123"
 #    ]
 #}
 
+# Activate Django-Heroku.
 django_heroku.settings(locals())
